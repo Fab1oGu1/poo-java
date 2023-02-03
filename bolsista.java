fabe426dded1356d9c2c6d51e87f12b9a3b912be
Fab1oGu1 <fabio2011141@gmail.com>
2023-02-03 19:02:47 -0300

    interface faculdade

---
+++ bolsista.java
@@ -0,0 +1,23 @@
+package com.mycompany.aula011;
+public class Bolsista extends Aluno {
+    private float bolsa;
+    
+    public void renovarBolsa() {
+        System.out.println("Renovando bolsa de " + this.getNome());
+    }
+    
+    @Override
+    public void pagarMensalidade() {
+        System.out.println("Renovando a bolda de " + this.getNome());
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+    
+    
+}
